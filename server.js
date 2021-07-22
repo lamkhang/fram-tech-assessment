03b7d3afbcb2c5a6b47136cf1058378e9a4db1cf
lamkhangkd <72370615+lbkhang@users.noreply.github.com>
2021-07-22 08:22:24 +0700

    update deploy Fram^ - Technical Assessment

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/"));
+app.use(express.static(__dirname + "/build/"));
 app.get(/.*/, function(req, res){
-    res.sendFile(__dirname + "/dist/index.html");
+    res.sendFile(__dirname + "/build/index.html");
 });
 
 app.listen(port);
